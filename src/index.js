15399ee21c883d734be12c9e2dc6f83bf632c862
el coruco <boris@gobiernofacil.com>
2018-11-28 17:59:14 -0600

    fix request test

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@
 // libraries
 import * as d3 from "d3";
 
+console.log("me");
+
 // endpoint
 const ENDPOINT = "https://demospdn.host/demo1/api/s1/declaraciones",
-      EXAMPLE  = "https://demospdn.host/demo1/api/s1/declaraciones?id=5bd8ec5180f35b060acf7033&perfil=perfil_3";
+      EXAMPLE  = "https://demospdn.host/demo1/api/s1/declaraciones?id=5bd8ec5680f35b060acf70da",
+      NAME     = "https://demospdn.host/demo1/api/s1/declaraciones?nombres=pedro&apellidos=MELIANI";
 
 
 // sample code
 let response = d3.json(EXAMPLE).then(d => {
-  console.log(d);
+  console.log("x: ", d);
 } );
