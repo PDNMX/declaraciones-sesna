c2c82c6b154005ffd5dba739957e133db33cbdf0
el coruco <boris@gobiernofacil.com>
2018-12-05 23:19:59 -0600

    add base pagination

--- src/index.js
+++ src/index.js
@@ -22,8 +22,30 @@ import Pasivos from './main-components/pasivos.vue';
 // Components LV3
 
 
-
+// constantes
 const API_PATH = "https://demospdn.host/demo1/api/v2/s1/declaraciones";
+
+const PROP_NAMES = {
+  institucionResponsable : "metadatos.institucion_responsable",
+  ente : "informacion_personal.datos_encargo_actual.ente_publico",
+  nombres : "informacion_personal.informacion_general.nombres",
+  apellido1 : "informacion_personal.informacion_general.primer_apellido",
+  apellido2 : "informacion_personal.informacion_general.segundo_apellido",
+  entidadID : "informacion_personal.datos_encargo_actual.direccion_encargo.entidad_federativa.cve_ent",
+  municipioID : "informacion_personal.datos_encargo_actual.direccion_encargo.municipio.cve_mun",
+  cargo : "informacion_personal.datos_encargo_actual.empleo_cargo_comision",
+  nivelGobierno : "informacion_personal.datos_encargo_actual.nivel_gobierno.codigo",
+  superficieConstruccion : "activos.bienes_inmuebles.superficie_construccion",
+  superficieTerreno : "activos.bienes_inmuebles.superficie_terreno",
+  adquisicionMetodo : "activos.bienes_inmuebles.forma_adquisicion.codigo",
+  adquisicionPrecio : "activos.bienes_inmuebles.precio_adquisicion.valor",
+  valorCatastral : "activos.bienes_inmuebles.valor_catastral",
+  nivelEncargo : "informacion_personal.datos_encargo_actual.nivel_encargo",
+  escolaridad : "informacion_personal.datos_curriculares.grado_maximo_escolaridad",
+  ingresoAnual : "ingresos.sueldos_salarios_publicos.ingreso_bruto_anual.valor",
+  ingresoMoneda : "ingresos.sueldos_salarios_publicos.ingreso_bruto_anual.moneda.codigo",
+  nacimiento : "informacion_personal.informacion_general.fecha_nacimiento"
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -38,6 +60,7 @@ Vue.use(VueRouter);
 // define las rutas
 const routes = [
   { path : '/', component : Busqueda},
+  { path : '/busqueda', component : Busqueda},
   { path : '/perfil/:id', component : Perfil}
 ];
 
@@ -69,6 +92,14 @@ const vueController = new Vue({
      */ 
     data : {
     	endpoint : API_PATH,
-    	profile  : null
+    	names : PROP_NAMES,
+    	profile  : null,
+    	fetchObj : {
+    		method : "POST",
+    		headers : {
+    			"Content-Type": "application/json; charset=utf-8"
+    		},
+    		body : null
+    	}
     }
 });
--- src/main-components/busqueda.vue
+++ src/main-components/busqueda.vue
@@ -1,6 +1,126 @@
 <template>
 	<div>
 		<h1>La búsqueda</h1>
-		
+		<form v-on:submit.prevent="search(0)">
+			<p>
+				nombres
+				<input type="text" name="names" v-model="names">
+			</p>
+
+			<p>
+				primer apellido
+				<input type="text" name="surname-a" v-model="surnameA">
+			</p>
+
+			<p>
+				oficina
+				<input type="text" name="office" v-model="office">
+			</p>
+
+			<p><input type="submit" name="submit" value="buscar"></p>
+		</form>
+
+		<table v-if="response && response.results.length">
+			<thead>
+				<tr>
+					<th>nombre</th>
+					<th>oficina</th>
+					<th>cargo</th>
+					<th>estado</th>
+					<th>municipio</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="compa in response.results">
+					<td>
+						<a :href="`/perfil/${compa._id}`">
+					    {{compa.informacion_personal.informacion_general.nombres}}
+					    {{compa.informacion_personal.informacion_general.primer_apellido}}
+					    {{compa.informacion_personal.informacion_general.segundo_apellido}}
+					  </a>
+					</td>
+					<td>
+					  {{compa.informacion_personal.datos_encargo_actual.ente_publico}}
+				  </td>
+					<td>
+					  {{compa.informacion_personal.datos_encargo_actual.empleo_cargo_comision}}
+				  </td>
+					<td>
+					{{compa.informacion_personal.datos_encargo_actual.direccion_encargo.entidad_federativa.nom_ent}}</td>
+					<td>
+					{{compa.informacion_personal.datos_encargo_actual.direccion_encargo.municipio.nom_mun}}</td>
+				</tr>
+			</tbody>
+
+			<ul v-if="total">
+				<li v-for="n in pages">
+				  <a href="#" v-on:click.prevent="search(n-1)">{{n}}</a>
+			  </li>
+			</ul>
+		</table>
+
+		<p v-if="response && !response.results.length">No hubo resultados :(</p>
 	</div>
 </template>
+
+<script>
+	export default {
+		data(){
+			return {
+				names    : "",
+				surnameA : "",
+				surnameB : "",
+				//location : "",
+				office   : "",
+				response : null,
+				pageSize : 10,
+				page     : 0, 
+				total    : 0
+			}
+		},
+		methods : {
+			search(page){
+				this.page     = page;
+				let connObj   = Object.assign({}, this.fetchObj);
+				connObj.body  = this.makeQuery();
+
+				fetch(this.endpoint, connObj)
+				  .then(response => response.json())
+				  .then(d => { 
+				  	this.response = d;
+				  	this.total    = d.total;
+
+				  	console.log("la respuesta completa: ", this.response);
+				  	console.log("un usuario: ", this.response.results[0]); 
+				  });
+			},
+
+			makeQuery(){
+				let searchObj   = {query : {}};
+				searchObj.limit = this.pageSize;
+				searchObj.skip  = this.page * this.pageSize;
+
+				if(this.names) searchObj.query[this.nameKeys.nombres] = this.names;
+				if(this.surnameA) searchObj.query[this.nameKeys.apellido1] = this.surnameA;
+				if(this.office) searchObj.query[this.nameKeys.institucionResponsable] = this.office;
+				return JSON.stringify(searchObj);
+			}
+		},
+		computed : {
+			endpoint(){
+				return this.$parent.endpoint;
+			},
+			nameKeys(){
+				return this.$parent.names;
+			},
+			fetchObj(){
+				return this.$parent.fetchObj;
+			},
+			pages(){
+				if(!this.total) return 0;
+				return Math.ceil(this.total / this.pageSize);
+			}
+		}
+	}
+</script>
+
