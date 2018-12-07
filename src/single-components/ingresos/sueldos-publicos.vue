66211bdf233a8ba6470d91f0bb385766de4cd401
El Santo <hugo.om@gmail.com>
2018-12-07 12:59:55 -0600

    update ingresos

--- src/single-components/ingresos/sueldos-publicos.vue
+++ src/single-components/ingresos/sueldos-publicos.vue
@@ -1,6 +1,18 @@
 <template>
-	<div>
-		<h3>Sueldos y Salarios por el Encargo Público</h3>
+	<div class="col-sm-9 col-sm-offset-3 sidecontent">
+		<h2>Sueldos y Salarios por el Encargo Público</h2>
+		
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="pdn_d_box" v-for="sueldo in items">
+					<p class="pdn_label">Ente público</p>
+					<p class="pdn_data_p">{{sueldo.ente_publico}}</p>
+					
+					
+					<p class="pdn_data_p">{{sueldo}}</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
