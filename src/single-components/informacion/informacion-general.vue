8fdea6429e4017cd5a7a004ac7c986dfc3ae70d0
hugovom <hugo.om@gmail.com>
2018-12-06 23:42:55 -0600

    update info gral

--- src/single-components/informacion/informacion-general.vue
+++ src/single-components/informacion/informacion-general.vue
@@ -1,11 +1,28 @@
 <template>
-	<div>
-		<h3>Información general</h3>
-		<p>
-		  correo:
-		  {{profile.informacion_personal.informacion_general.correo_electronico.laboral}}
-	  </p>
+<div>
+	<div class="col-sm-9 col-sm-offset-3 sidecontent">
+		<h2>Información general</h2>
+		
+		<div class="row">
+			<div class="col-sm-7">
+				<div class="pdn_d_box">
+					<p class="pdn_label">Nacionalidad</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.nacionalidades}}</p>
+					<p class="pdn_label">Lugar de nacimiento</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.entidad_federativa_nacimiento}}</p>
+				</div>
+			</div>
+			<div class="col-sm-5">
+				<div class="pdn_d_box">
+					<p class="pdn_label">Estado Civil</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.estado_civil}}</p>
+					<p class="pdn_label">Regimen matrimonial</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.regimen_matrimonial}}</p>
+				</div>
+			</div>
+		</div>
 	</div>
+</div>
 </template>
 
 <script>
