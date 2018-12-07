d799439099565dab8683024bfc323fcfcfaaa403
hugovom <hugo.om@gmail.com>
2018-12-06 23:53:07 -0600

    update puesto actual

--- src/single-components/informacion/puesto-actual.vue
+++ src/single-components/informacion/puesto-actual.vue
@@ -1,6 +1,32 @@
 <template>
-	<div>
-		<h3>Puesto actual</h3>
+<div class="col-sm-9 col-sm-offset-3 sidecontent">
+		<h2>Puesto actual</h2>
+		
+		<div class="row">
+			<div class="col-sm-8">
+				<div class="pdn_d_box">
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.empleo_cargo_comision}}</p>
+					<p class="pdn_label">Área de adscripción</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.area_adscripcion}}</p>
+					<p class="pdn_label">Dependencia</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.ente_publico}}</p>
+					<p class="pdn_label">Nivel de gobierno</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.nivel_gobierno}}</p>
+					<p class="pdn_label">Poder</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.poder_juridico}}</p>
+					<p class="pdn_label">Sector/Industria</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.datos_encargo_actual.sector_industria}}</p>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="pdn_d_box">
+					<p class="pdn_label">Estado Civil</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.estado_civil}}</p>
+					<p class="pdn_label">Regimen matrimonial</p>
+					<p class="pdn_data_p">{{profile.informacion_personal.informacion_general.regimen_matrimonial}}</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
