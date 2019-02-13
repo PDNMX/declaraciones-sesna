0826572a01aae1fb036f2d2d4ae94df88ffb6220
El Santo <hugo.om@gmail.com>
2019-02-13 13:05:04 -0600

    update perfil component & css

--- src/main-components/Perfil.js
+++ src/main-components/Perfil.js
@@ -56,7 +56,7 @@ class Perfil extends Component{
 		  			all = all.concat(this.state.profile.ingresos[ConstClass.Incomefields[i]].map(d => d.ingreso_bruto_anual));
 		  		}
 		  	}
-		  	
+
 		  	all = all.filter(d => d.moneda.codigo == "MXN").map(d => d.valor);
 
 		  	return all.reduce(reducer).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
@@ -66,33 +66,37 @@ class Perfil extends Component{
 
 	render(){
 		if(!this.state.profile){
-			console.log("nel"); 
+			console.log("nel");
 			return null;
-	  } 
+	  }
 		return(
 			<div>
 <div v-if="profile">
 
 	<div className="row pdn_card_m_info">
-		<div className="col-sm-5">
+		<div className="col-sm-6">
 			<div className="row">
 				<div className="col-sm-4">
-					<img src="/img/user.svg" />
+					<img src="/img/avatar.svg" />
 				</div>
 				<div className="col-sm-8">
 					<h1>{this.state.profile.informacion_personal.informacion_general.nombres} {this.state.profile.informacion_personal.informacion_general.primer_apellido} {this.state.profile.informacion_personal.informacion_general.segundo_apellido}</h1>
-					<p>{this.state.profile.informacion_personal.informacion_general.correo_electronico.laboral}</p>
-				</div>					
+					<p className="pdn_mail">{this.state.profile.informacion_personal.informacion_general.correo_electronico.laboral}</p>
+					<p className="pdn_label">ENCARGO ACTUAL</p>
+					<p>{this.state.profile.informacion_personal.datos_encargo_actual.empleo_cargo_comision}</p>
+				</div>
 			</div>
 		</div>
-		<div className="col-sm-4">
-			<h3>{this.state.profile.informacion_personal.datos_encargo_actual.empleo_cargo_comision}</h3>
-			<p>{this.state.profile.informacion_personal.datos_encargo_actual.area_adscripcion}<br/>
-				<strong>{this.state.profile.informacion_personal.datos_encargo_actual.ente_publico}</strong></p>
-		</div>
 		<div className="col-sm-3">
+
+			<p className="pdn_label">ÁREA DE ADSCRIPCIÓN</p>
+			<p>{this.state.profile.informacion_personal.datos_encargo_actual.area_adscripcion}</p>
+			<p className="pdn_label">DEPENDENCIA</p>
+			<p><strong>{this.state.profile.informacion_personal.datos_encargo_actual.ente_publico}</strong></p>
+		</div>
+		<div className="col-sm-3 right">
+				<p className="pdn_label">INGRESOS ANUALES NETOS</p>
 				<h3><span className="pdn_score">${this.ingresosAnualesNetos()}</span></h3>
-				<p>Ingresos anuales netos</p>
 				<p><small>Actualización: {this.state.profile.informacion_personal.informacion_general.fecha_declaracion} </small></p>
 		</div>
 	</div>
@@ -102,23 +106,23 @@ class Perfil extends Component{
 			<nav className="pdn_main_nav">
 				<ul>
 					<li>
-						<Link to={`/perfil/${this.props.match.params.id}/informacion`}>Información</Link>
+						<Link to={`/perfil/${this.props.match.params.id}/informacion`}><b className="pdn_i_ info"></b>Información</Link>
 					</li>
-				
+
 					<li>
-						<Link to={`/perfil/${this.props.match.params.id}/intereses`}>Intereses</Link>
+						<Link to={`/perfil/${this.props.match.params.id}/intereses`}><b className="pdn_i_ intereses"></b>Intereses</Link>
 					</li>
-				
+
 					<li>
-						<Link to={`/perfil/${this.props.match.params.id}/ingresos`}>Ingresos</Link>
+						<Link to={`/perfil/${this.props.match.params.id}/ingresos`}><b className="pdn_i_ ingresos"></b>Ingresos</Link>
 					</li>
-				
+
 					<li>
-						<Link to={`/perfil/${this.props.match.params.id}/activos`}>Activos</Link>
+						<Link to={`/perfil/${this.props.match.params.id}/activos`}><b className="pdn_i_ activos"></b>Activos</Link>
 					</li>
-				
+
 					<li>
-						<Link to={`/perfil/${this.props.match.params.id}/pasivos`}>Pasivos</Link>
+						<Link to={`/perfil/${this.props.match.params.id}/pasivos`}><b className="pdn_i_ pasivos"></b>Pasivos</Link>
 					</li>
 				</ul>
 			</nav>
@@ -129,8 +133,8 @@ class Perfil extends Component{
 	<div className="row pnd_box">
 		<Informacion profile={this.state.profile} />
 	</div>
-		
-			
+
+
 </div>
 
 </div>
