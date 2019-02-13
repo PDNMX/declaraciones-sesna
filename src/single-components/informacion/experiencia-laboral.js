f8788c42c1bb3c161616c57ec47eecc391419a17
El Santo <hugo.om@gmail.com>
2019-02-13 17:08:27 -0600

    update experiencia laboral

--- src/single-components/informacion/experiencia-laboral.js
+++ src/single-components/informacion/experiencia-laboral.js
@@ -12,29 +12,38 @@ class InfoExperiencia extends Component{
 		return(
 			<div className="col-sm-9 col-sm-offset-3 sidecontent">
 		<h2>Experiencia laboral</h2>
-		
+
 		<div className="row">
 			<div className="col-sm-12">
 			  { this.props.items.map( (job, i) =>
 				<div className="pdn_d_box" key={"job-" + i}>
 					<div className="row">
 						<div className="col-sm-6">
-							<p className="pdn_data_p">Del {job.fecha_ingreso} al {job.fecha_salida}</p>
+							<p className="pnd_box_note pdn_gray">Del {job.fecha_ingreso} al {job.fecha_salida}</p>
 						</div>
 						<div className="col-sm-6">
-							<p className="pdn_data_p">Ámbito {job.ambito.valor}</p>
+							<p className="right"><span className={ 'ambito ' + job.ambito.valor.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")}>Ámbito {job.ambito.valor}</span></p>
 						</div>
 					</div>
-					<p className="pdn_data_p">{job.cargo_puesto}</p>
-					<div className="row">
+					<h3 className="pdn_data_p">{job.cargo_puesto}</h3>
+					<div className="row pdn_border">
 						<div className="col-sm-6">
 							<p className="pdn_label">Institución</p>
-							<p className="pdn_data_p">{job.nombre_institucion}</p>
+							<p className="pdn_data_p">{job.nombre_institucion}<br/>
+							<span className="pnd_box_note">{ job.direccion.vialidad.tipo_vial }	{ job.direccion.vialidad.nom_vial }	#{ job.direccion.numExt }
+							<span>{ job.direccion.numExt ? ", int. #" + job.direccion.numExt : "" }</span><br/>
+							{ job.direccion.localidad.nom_loc }, { job.direccion.municipio.nom_mun }<br/>
+							{ job.direccion.entidad_federativa.nom_ent }. C.P. { job.direccion.cp }
+							</span>
+							</p>
 						</div>
 						<div className="col-sm-6">
 							<p className="pdn_label">Unidad Administrativa / Área</p>
 							<p className="pdn_data_p">{job.unidad_administrativa}</p>
 						</div>
+					</div>
+					{/* ends row */}
+					<div className="row pdn_border">
 						<div className="col-sm-6">
 							<p className="pdn_label">Nivel de gobierno</p>
 							<p className="pdn_data_p">{job.nivel_gobierno.valor}</p>
@@ -43,15 +52,19 @@ class InfoExperiencia extends Component{
 							<p className="pdn_label">Poder</p>
 							<p className="pdn_data_p">{job.poder_ente.valor}</p>
 						</div>
+					</div>
+					{/* ends row */}
+					<div className="row pdn_border">
 						<div className="col-sm-6">
 							<p className="pdn_label">Sector/Industria</p>
 							<p className="pdn_data_p">{job.sector_industria.valor}</p>
 						</div>
 						<div className="col-sm-6">
 							<p className="pdn_label">Nivel del encargo</p>
-							<p className="pdn_data_p">{job.poder_ente.valor}</p>
+							<p className="pdn_data_p">{job.nivel_encargo}</p>
 						</div>
 					</div>
+					{/* ends row */}
 					<p className="pdn_label">Funciones principales</p>
 					{ job.funciones_principales.map( (d, j) =>
 					<p className="pdn_data_p" key={"fun-prin-" + j}>{d.valor}</p>
@@ -59,7 +72,7 @@ class InfoExperiencia extends Component{
 				</div>
 			  )}
 			</div>
-	
+
 		</div>
 	</div>
 		);
