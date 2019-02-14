56c623030597a97f21c77f9b75dbdb26be7e90c1
hugovom <hugo.om@gmail.com>
2019-02-13 22:00:56 -0600

    update empresas

--- src/single-components/intereses/empresas.js
+++ src/single-components/intereses/empresas.js
@@ -27,24 +27,58 @@ class InteresesEmpresas extends Component{
   render(){
     return(
       <div className="col-sm-9 col-sm-offset-3 sidecontent">
-    <h2>Empresas sociedades asociaciones</h2>
+      <h2>Empresas, sociedades o asociaciones ({this.items().length})</h2>
+      <div className="row">
+				<div className="col-sm-12">
+					<div className="pdn_d_box">
+						<div className="pdn_bar_container">
+							<div className="pdn_bar conyuge"></div>
+						</div>
+						<p className="pdn_graph_label"><b className="pdn_graph_label_item label conyuge"></b> Cónyugue</p>
+					</div>
+				</div>
+			</div>
     <div className="row">
       <div className="col-sm-12">
-        { this.items().map( (interes, i) => 
+        { this.items().map( (interes, i) =>
         <div className="pdn_d_box" v-for="interes in items">
-        <p className="pdn_label">Nombre de la empresa, sociedad o asociación</p>
-        <p className="pdn_data_p">{interes.nombre_empresa_sociedad_asociacion}</p>
-        <p className="pdn_label">País</p>
-        <p className="pdn_data_p">{interes.pais_registro.valor}</p>
-        <p className="pdn_label">Fecha de constitución</p>
-        <p className="pdn_data_p">{interes.fecha_constitucion}</p>
-        <p className="pdn_label">Número de registro</p>
-        <p className="pdn_data_p">{interes.numero_registro}</p>
-        <p className="pdn_label">R.F.C</p>
-        <p className="pdn_data_p">{interes.rfc}</p>
+            <div class="row pdn_border">
+              {/* empresa, sociedad o asociación */}
+              <div className="col-sm-9">
+                <p className="pdn_label">Nombre de la empresa, sociedad o asociación</p>
+                <h3>{interes.nombre_empresa_sociedad_asociacion}</h3>
+                <p className="pdn_data_p pnd_box_note pdn_gray">{interes.domicilio.vialidad.tipo_vial+' '+interes.domicilio.vialidad.nom_vial+' No.' +interes.domicilio.numExt+ ' No. Int.'+interes.domicilio.numInt}
+                 <br/> {interes.domicilio.localidad.nom_loc+', '+interes.domicilio.municipio.nom_mun+', '+interes.domicilio.entidad_federativa.nom_ent+', '+interes.domicilio.pais.valor+' C.P. '+interes.domicilio.cp }</p>
+              </div>
+              {/* constitución */}
+              <div className="col-sm-3">
+                <p className="pdn_label">Fecha de constitución</p>
+                <p className="pdn_data_p">{interes.fecha_constitucion}</p>
+              </div>
+            </div>
+            {/* row ends*/}
+            <div className="row pdn_border">
+              {/* país*/}
+              <div className="col-sm-4">
+                <p className="pdn_label">País</p>
+                <p className="pdn_data_p">{interes.pais_registro.valor}</p>
+              </div>
+              {/* RFC*/}
+              <div className="col-sm-4">
+                <p className="pdn_label">R.F.C</p>
+                <p className="pdn_data_p">{interes.rfc}</p>
+              </div>
+              {/* # registro */}
+              <div className="col-sm-4">
+                <p className="pdn_label">Número de registro</p>
+                <p className="pdn_data_p">{interes.numero_registro}</p>
+              </div>
+            </div>
+
+
+
+
         <p className="pdn_label">Domicilio</p>
-        <p className="pdn_data_p">{interes.domicilio.vialidad.tipo_vial+' '+interes.domicilio.vialidad.nom_vial+' No.' +interes.domicilio.numExt+ ' No. Int.'+interes.domicilio.numInt
-        + ' '+interes.domicilio.localidad.nom_loc+', '+interes.domicilio.municipio.nom_mun+', '+interes.domicilio.entidad_federativa.nom_ent+', '+interes.domicilio.pais.valor+' C.P. '+interes.domicilio.cp }</p>
         <p className="pdn_label">Rol</p>
         <p className="pdn_data_p">{interes.rol}</p>
         <p className="pdn_label">Actividad económica</p>
