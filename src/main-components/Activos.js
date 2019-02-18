71542716dfc18727566e624910152606d71b2759
El Santo <hugo.om@gmail.com>
2019-02-18 12:56:24 -0600

    update navigation

--- src/main-components/Activos.js
+++ src/main-components/Activos.js
@@ -27,58 +27,58 @@ class Activos extends Component{
 	<div class="col-sm-3 sidebar">
 		<h2>Activos</h2>
 		<ul>
-			
+
 			<li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos`}>
-	  	    Bienes inmuebles
-	  	  </Link>
-	    </li>
-	    
-	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/bienes-muebles`}>
-	  	    Bienes muebles registrables
+	  	  <Link className={ !section ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos`}>
+	  	    Bienes inmuebles <span>{this.props.profile.activos.bienes_inmuebles.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/bienes-muebles-no-registrables`}>
-	  	    Bienes muebles no registrables
+	  	  <Link className={ section == "bienes-muebles" ?"router-link-exact-active router-link-active" : ""}  to={`/perfil/${this.props.profile._id}/activos/bienes-muebles`}>
+	  	    Bienes muebles registrables <span>{this.props.profile.activos.bienes_muebles_registrables.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/inversiones`}>
-	  	    Inversiones
+	  	  <Link className={ section == "bienes-muebles-no-registrables" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/bienes-muebles-no-registrables`}>
+	  	    Bienes muebles no registrables <span>{this.props.profile.activos.bienes_muebles_no_registrables.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/efectivo-y-metales`}>
-	  	    Efectivo y metales
+	  	  <Link className={ section == "inversiones" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/inversiones`}>
+	  	    Inversiones <span>{this.props.profile.activos.inversiones_cuentas_valores.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/fideicomisos`}>
-	  	    Fideicomisos
+	  	  <Link className={ section == "efectivo-y-metales" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/efectivo-y-metales`}>
+	  	    Efectivo y metales <span>{this.props.profile.activos.efectivo_metales.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/bienes-intangibles`}>
-	  	    Bienes intangibles
+	  	  <Link className={ section == "fideicomisos" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/fideicomisos`}>
+	  	    Fideicomisos <span>{this.props.profile.activos.fideicomisos.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/cuentas-por-cobrar`}>
-	  	    Cuentas por cobrar
+	  	  <Link className={ section == "bienes-intangibles" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/bienes-intangibles`}>
+	  	    Bienes intangibles <span>{this.props.profile.activos.bienes_intangibles.length}</span>
 	  	  </Link>
 	    </li>
 
 	    <li>
-	  	  <Link to={`/perfil/${this.props.profile._id}/activos/beneficios-en-especie`}>
-	  	    Uso o Beneficios en Especie Propiedad de un Tercero
+	  	  <Link className={ section == "cuentas-por-cobrar" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/cuentas-por-cobrar`}>
+	  	    Cuentas por cobrar <span>{this.props.profile.activos.cuentas_por_cobrar.length}</span>
+	  	  </Link>
+	    </li>
+
+	    <li>
+	  	  <Link className={ section == "beneficios-en-especie" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/activos/beneficios-en-especie`}>
+	  	    Uso o Beneficios en Especie Propiedad de un Tercero <span>{this.props.profile.activos.uso_especie_propiedad_tercero.length}</span>
 	  	  </Link>
 	    </li>
 
