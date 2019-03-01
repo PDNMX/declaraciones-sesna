4250f7312687e9b4c5efa23b4fce873ef4488803
El Santo <hugo.om@gmail.com>
2019-03-01 14:23:22 -0600

    add sub nav to state #2

--- src/single-components/stats/State.js
+++ src/single-components/stats/State.js
@@ -6,384 +6,106 @@
   ////////////////////////////////////////////////////////////////////////////////
 */
 import React, {Component} from "react";
-import * as ConstClass from  '../../ConstValues.js';
-import ChartistGraph from 'react-chartist';
-import "../../css/chartist.min.css"
+import { Link } from 'react-router-dom';
 
+import FuncionariosPorEntidadTotal from './state/FuncionariosPorEntidadTotal';
+import FuncionariosPorEntidadPorcentaje from './state/FuncionariosPorEntidadPorcentaje';
+
+import FuncionariosPorEntidadEdad from './state/FuncionariosPorEntidadEdad';
+import FuncionariosPorEntidadEdadPorcentaje from './state/FuncionariosPorEntidadEdadPorcentaje';
+
+import FuncionariosPorEntidadEducacion from './state/FuncionariosPorEntidadEducacion';
+import FuncionariosPorEntidadEducacionPorcentaje from './state/FuncionariosPorEntidadEducacionPorcentaje';
+
+/*
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  // DEFINE LA CLASE PRINCIPAL
+  //
+  ////////////////////////////////////////////////////////////////////////////////
+*/
 class State extends Component{
-	constructor(){
-		super();
 
-		this.state = {
-			ageFrom : 18,
-			ageTo   : 98,
-			step    : 10,
-			data    : null,
-			fake    : {
-				labels : ["Puebla", "Ciudad de México", "Guanajuato", "Nuevo León", "Zacatecas", "Queretaro", "Tlaxcala"],
-				series : [ [200, 100, 400, 123, 500, 200, 300] ]
-			},
-			fake5    : {
-				labels : ["Puebla", "Ciudad de México", "Guanajuato", "Nuevo León", "Zacatecas", "Queretaro", "Tlaxcala"],
-				series : [ [10000, 20000, 20000, 15000, 15000, 15000, 5000],
-				           [8000, 15000, 12000, 11000, 10000, 7000, 7000],
-				           [12000, 22000, 18000, 12000, 11000, 10000, 4000],
-				           [2000, 32000, 10000, 11000, 10000, 1000, 23000],
-				           [10000, 20000, 10000, 11000, 1000, 14000, 12000],
-				           [9000, 11000, 2000, 10000, 6000, 5000, 9000],
-				           [2000, 20000, 11000, 5000, 17000, 12000, 14000]
-				         ]
-			},
-			fake6 : {
-				series : [12, 15, 13, 20, 10, 20, 10]
-			},
-			labels    : {
-				_labels  : ["20-30", "30-40", "40-50", "50-60", "60-70", "70-80", "80-90"],
-				__labels : ["Primaria", "Secundaria", "Preparatoria", "Licenciatura", "Maestría", "Doctorado", "post-doc"],
-				___labels : ["Puebla", "Ciudad de México", "Guanajuato", "Nuevo León", "Zacatecas", "Queretaro", "Tlaxcala"],
-				labels : ["Federal", "Estatal", "Municipal"],
-			},
-
-			donutOptions : {donut: true, donutWidth: 30}
-		}
-
-		this.getInfo  = this.getInfo.bind(this);
-		this.makeData = this.makeData.bind(this);
-
-		let promises = this.makeData();
-
-		Promise.all(promises.map(d => d.promise)).then(d => {
-			console.log("res:", d);
-		});
-
-	};
+	/*
+	 * R E N D E R
+	 * ----------------------------------------------------------------------
+	 */
 
 	render(){
-		let st = this.state;
+		let cat = this.props.match.params.categoria;
 		return(
 			<div>
 				<div className="col-sm-3 sidebar">
 					<h2>Por entidad</h2>
+					<ul>
+				  	<li>
+				  	  <Link to="/estadistica/entidad-federativa" className={ !cat ? "router-link-exact-active router-link-active" : "" }>Funcionarios por entidad</Link>
+				  	</li>
+				  	<li>
+				  	  <Link to="/estadistica/entidad-federativa/gobierno-y-edad" className={ cat == "gobierno-y-edad" ? "router-link-exact-active router-link-active" : "" }>Funcionarios por entidad y edad</Link>
+				  	</li>
+				  	<li>
+				  	  <Link to="/estadistica/entidad-federativa/gobierno-y-educacion" className={ cat == "gobierno-y-educacion" ? "router-link-exact-active router-link-active" : "" }>Funcionarios por entidad y educación</Link>
+				  	</li>
+				  </ul>
 				</div>
 
 				<div className="col-sm-9 col-sm-offset-3 sidecontent">
-				<div className="row">
-					<div className="col-sm-12">
-						<div className="pdn_d_box">
-				<h2>Funcionarios por entidad federativa (total)</h2>
-				<ChartistGraph data={st.fake} type={"Bar"} />
-							</div>
-						</div>
-					</div>
-			<div className="row">
-				<div className="col-sm-12">
-					<div className="pdn_d_box">
-				<h2>Funcionarios por entidad federativa (porcentaje)</h2>
-				<ChartistGraph data={ { series : st.fake.series[0]} } type={"Pie"} options={st.donutOptions} />
-
-				<ul className="list_inline">
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#d0001c"} }>
-				    </span> {st.labels.___labels[0]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#e95a55"} }>
-				    </span> {st.labels.___labels[1]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#edcb4f"} }>
-				    </span> {st.labels.___labels[2]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#ca7c24"} }>
-				    </span> {st.labels.___labels[3]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#453d3f"} }>
-				    </span> {st.labels.___labels[4]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#589634"} }>
-				    </span> {st.labels.___labels[5]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#2d1a9c"} }>
-				    </span> {st.labels.___labels[6]}
-				  </li>
-				</ul>
-				</div>
-			</div>
-		</div>
-<div className="row">
-	<div className="col-sm-12">
-		<div className="pdn_d_box">
-
-				<h2>Funcionarios por estado seleccionado y rango de edad</h2>
-				<ChartistGraph data={st.fake5} type={"Line"} />
-
-				<ul className="list_inline">
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#d0001c"} }>
-				    </span> {st.labels._labels[0]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#e95a55"} }>
-				    </span> {st.labels._labels[1]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#edcb4f"} }>
-				    </span> {st.labels._labels[2]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#ca7c24"} }>
-				    </span> {st.labels._labels[3]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#453d3f"} }>
-				    </span> {st.labels._labels[4]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#589634"} }>
-				    </span> {st.labels._labels[5]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#2d1a9c"} }>
-				    </span> {st.labels._labels[6]}
-				  </li>
-				</ul>
-				</div>
-			</div>
-		</div>
-<div className="row">
-	<div className="col-sm-12">
-		<div className="pdn_d_box">
-
-				<h2>Funcionarios por estado seleccionado y rango de edad (porcentaje)</h2>
-				<nav class="pdn_viz">
-				<ul>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[0]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[1]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[2]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[3]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[4]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[5]}</p>
-				  </li>
-				  <li>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[6]}</p>
-				  </li>
-				</ul>
-				</nav>
-				<ul className="list_inline">
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#d0001c"} }>
-				    </span> {st.labels._labels[0]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#e95a55"} }>
-				    </span> {st.labels._labels[1]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#edcb4f"} }>
-				    </span> {st.labels._labels[2]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#ca7c24"} }>
-				    </span> {st.labels._labels[3]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#453d3f"} }>
-				    </span> {st.labels._labels[4]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#589634"} }>
-				    </span> {st.labels._labels[5]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#2d1a9c"} }>
-				    </span> {st.labels._labels[6]}
-				  </li>
-				</ul>
+					{ this.porEntidad() }
 
 				</div>
-			</div>
-		</div>
-
-<div className="row">
-	<div className="col-sm-12">
-		<div className="pdn_d_box">
-
-				<h2>Funcionarios por estado seleccionado y nivel educativo</h2>
-				<ChartistGraph data={st.fake5} type={"Line"} />
-
-				<ul className="list_inline">
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#d0001c"} }>
-				    </span> {st.labels.__labels[0]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#e95a55"} }>
-				    </span> {st.labels.__labels[1]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#edcb4f"} }>
-				    </span> {st.labels.__labels[2]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#ca7c24"} }>
-				    </span> {st.labels.__labels[3]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#453d3f"} }>
-				    </span> {st.labels.__labels[4]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#589634"} }>
-				    </span> {st.labels.__labels[5]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#2d1a9c"} }>
-				    </span> {st.labels.__labels[6]}
-				  </li>
-				</ul>
-				</div>
-			</div>
-		</div>
-		<div className="row">
-		<div className="col-sm-12">
-		<div className="pdn_d_box">
-
-				<h2>Funcionarios por estado seleccionado y nivel educativo (porcentaje)</h2>
-				<nav class="pdn_viz">
-				<ul>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[0]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[1]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[2]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[3]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[4]}</p>
-				  </li>
-				  <li style={ {float: "left"} }>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[5]}</p>
-				  </li>
-				  <li>
-				    <ChartistGraph data={ { series : st.fake6.series} } type={"Pie"} options={st.donutOptions} />
-				    <p>{st.labels.___labels[6]}</p>
-				  </li>
-				</ul>
-				</nav>
-				<ul className="list_inline">
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#d0001c"} }>
-				    </span> {st.labels.__labels[0]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#e95a55"} }>
-				    </span> {st.labels.__labels[1]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#edcb4f"} }>
-				    </span> {st.labels.__labels[2]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#ca7c24"} }>
-				    </span> {st.labels.__labels[3]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#453d3f"} }>
-				    </span> {st.labels.__labels[4]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#589634"} }>
-				    </span> {st.labels.__labels[5]}
-				  </li>
-				  <li>
-				    <span style={ {display: "inline-block", width: "1em", height: "1em", background: "#2d1a9c"} }>
-				    </span> {st.labels.__labels[6]}
-				  </li>
-				</ul>
-
-				</div>
-			</div>
-		</div>
-
-			</div>
 			</div>
 		);
 	}
+	/*
+	 * T E M P L A T E S
+	 * ----------------------------------------------------------------------
+	 */
 
-	getInfo(_from, _to){
-	  let connObj = Object.assign({}, ConstClass.fetchObj);
-
-	  connObj.body = this.makeQuery(_from, _to);
-
-	  return fetch(ConstClass.endpoint, connObj)
-          .then(response => response.json())
-          .then(d => {
-            return d.total;
-          });
-  }
-
-  makeData(){
-  	let currentYear = (new Date()).getFullYear(),
-  	    _from = d => `${d}-01-01`,
-  	    _to   = d => `${d}-12-31`,
-  	    st    = this.state,
-  	    i     = currentYear - st.ageFrom,
-  	    res   = [],
-  	    _f, _t;
-
-  	while(i > currentYear - st.ageTo){
-  		res.push({
-  			label   : (currentYear - i) + " - " + (currentYear - i+st.step),
-  			promise : this.getInfo(_from(i-10),  _to(i)).catch(error => { return error }),
-  			from    : _from(i-10),
-  			to      :  _to(i)
-  		});
-  		i-= st.step;
-  	}
-
-  	return res;
-  }
-
-  makeQuery(_from, _to){
-  	let str     = ConstClass.PROP_NAMES.nacimiento,
-  	    search  = {query : {}, limit : 2};
-
-	  search.query[str] = {desde : _from, hasta : _to};
-	  return JSON.stringify(search);
-  }
+	/*
+	/  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+	/
+	/
+	/
+	/  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+	*/
+	porEntidad(){
+		let cat = this.props.match.params.categoria;
+		if(!cat){
+			return(
+			  <div>
+			    <FuncionariosPorEntidadTotal />
+			    <FuncionariosPorEntidadPorcentaje />
+			  </div>
+			);
+	  }
+	  else if(cat == "gobierno-y-edad"){
+	  	return(
+	  		<div>
+					<FuncionariosPorEntidadEdad />
+					<FuncionariosPorEntidadEdadPorcentaje />
+	  		</div>
+	  	);
+	  }
+	  else if(cat == "gobierno-y-educacion"){
+	  	return(
+				<div>
+					<FuncionariosPorEntidadEducacion />
+					<FuncionariosPorEntidadEducacionPorcentaje />
+	  		</div>
+	  	);
+	  }
+	  else{
+	  	return null;
+	  }
+	}
 }
 
+/*
+  ////////////////////////////////////////////////////////////////////////////////
+  //
+  // REGRESA EL COMPONENTE
+  //
+  ////////////////////////////////////////////////////////////////////////////////
+*/
 export default State;
