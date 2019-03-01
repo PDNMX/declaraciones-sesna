e8d172024b2d8b844d61b2be47ab904f8cea866c
el coruco <boris@gobiernofacil.com>
2019-03-01 11:50:33 -0600

    example for material ui

--- src/single-components/BusquedaFromMaterialUI.js
+++ src/single-components/BusquedaFromMaterialUI.js
@@ -5,11 +5,10 @@
   //
   ////////////////////////////////////////////////////////////////////////////////
 */
-import React, {Component, Fragment}  from "react";
+import React, {Component}  from "react";
 
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
-
+import {Typography, TextField, Grid, RadioGroup, Radio, FormControlLabel, FormGroup, Button} from '@material-ui/core';
+import red from '@material-ui/core/colors/red';
 /*
   ////////////////////////////////////////////////////////////////////////////////
   //
@@ -18,13 +17,16 @@ import Grid from '@material-ui/core/Grid';
   ////////////////////////////////////////////////////////////////////////////////
 */
 class BusquedaFromMaterialUI extends Component {
+  
+  state = {
+          names : "",
+      surname_a : "",
+         office : "", 
+          nivel : ""
+  };
+
   render(){
-    const { classes } = this.props;
-
-    console.log("clasess:", classes);
-
     return(
-      <Fragment>
         <form>
           <Grid container spacing={16}>
             <Grid item xs={12} sm={4}>
@@ -54,9 +56,63 @@ class BusquedaFromMaterialUI extends Component {
               />
             </Grid>
           </Grid>
+
+          <Grid container spacing={16}>
+            <Grid item xs={12} sm={8}>
+              <Grid container spacing={16}>
+                <Grid item xs={12} sm={3}>
+                  <Typography variant="body1"> Nivel de gobierno:</Typography>
+                </Grid>
+              
+
+                <Grid item xs={12} sm={9}>
+                  <Grid container spacing={16}>
+                  <Grid item xs={12} sm={3}>
+                    <FormControlLabel value="" control={<Radio checked={this.state.nivel === ""} name="nivel" onChange={this.handleInputChange} />} label="Todos" />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControlLabel value="FED" control={<Radio checked={this.state.nivel === "FED"} name="nivel" onChange={this.handleInputChange} />} label="Federal" />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControlLabel value="EST" control={<Radio checked={this.state.nivel === "EST"} name="nivel" onChange={this.handleInputChange} />} label="Estatal" />
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <FormControlLabel value="MUN" control={<Radio checked={this.state.nivel === "MUN"} name="nivel" onChange={this.handleInputChange} />} label="Municipal" />
+                  </Grid>
+                  </Grid>
+
+                </Grid>
+
+
+              </Grid>
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Button variant="contained" color="primary">Buscar</Button>
+            </Grid>
+
+          </Grid>
         </form>
-      </Fragment>
     );
+  }
+
+  /*
+  /  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+  /
+  /  genera una sola "source of truth" para los 
+  /  valores del formulario. En vue es lo de 
+  /  v-model
+  /
+  /  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+  */
+  handleInputChange = event => {
+    const target = event.target;
+    const value  = target.value;
+    const name   = target.name;
+
+    this.setState({
+      [name]: value
+    });
   }
 }
 
