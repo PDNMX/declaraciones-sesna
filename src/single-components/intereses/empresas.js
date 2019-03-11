a02304088a2cbc83d9f314661dd713ab79372714
El Santo <hugo.om@gmail.com>
2019-03-11 17:11:12 -0600

    update empresas #4

--- src/single-components/intereses/empresas.js
+++ src/single-components/intereses/empresas.js
@@ -6,6 +6,7 @@
   ////////////////////////////////////////////////////////////////////////////////
 */
 import React, {Component} from "react";
+import {Typography, Grid, Button, Paper} from '@material-ui/core';
 
 /*
 	////////////////////////////////////////////////////////////////////////////////
@@ -38,98 +39,94 @@ class InteresesEmpresas extends Component{
    */
   render(){
     return(
-      <div className="col-sm-9 col-sm-offset-3 sidecontent">
-      <h2>Empresas, sociedades o asociaciones ({this.items().length})</h2>
-      <div className="row">
-				<div className="col-sm-12">
-					<div className="pdn_d_box">
-						<div className="pdn_bar_container">
-							<div className="pdn_bar conyuge"></div>
-						</div>
-						<p className="pdn_graph_label"><b className="pdn_graph_label_item label conyuge"></b> Cónyugue</p>
-					</div>
-				</div>
-			</div>
-    <div className="row">
-      <div className="col-sm-12">
-        {/* comienza box*/ }
-        { this.state.items.map( (interes, i) =>
-        <div className="pdn_d_box" key={"interes-" + i} id={"interes-" + i}>
-            <div className="row pdn_border">
-              <div className="col-sm-6">
-                <p><span className="label declarante"> Declarante</span></p>
-              </div>
-              <div className="col-sm-6 right">
-                <a onClick={(e) => this.toggl(interes, i, e)} heref="#" className={"pdn_arrow " + (interes.show ?  "close" : "open")}></a>
-              </div>
-            </div>
-            {/* div close/open */}
-            <div style={ {display : (interes.show ? "block" : "none")} }>
-              <div className="row pdn_border">
-                {/* empresa, sociedad o asociación */}
-                <div className="col-sm-9">
-                  <p className="pdn_label">Nombre de la empresa, sociedad o asociación</p>
-                  <h3> { interes.nombre_empresa_sociedad_asociacion } </h3>
-                  <p className="pdn_data_p pnd_box_note pdn_gray">{interes.domicilio.vialidad.tipo_vial+' '+interes.domicilio.vialidad.nom_vial+' No.' +interes.domicilio.numExt+ ' No. Int.'+interes.domicilio.numInt}
-                   <br/> { interes.domicilio.localidad.nom_loc+', '+interes.domicilio.municipio.nom_mun+', '+interes.domicilio.entidad_federativa.nom_ent+', '+interes.domicilio.pais.valor+' C.P. '+interes.domicilio.cp } </p>
-                </div>
-                {/* constitución */}
-                <div className="col-sm-3">
-                  <p className="pdn_label">Fecha de constitución</p>
-                  <p className="pdn_data_p">{interes.fecha_constitucion} </p>
-                </div>
-              </div>
+      <Grid container spacing={24} direction={'row-reverse'} className="col-sm-offset-3 sidecontent">
+      <Grid item sm={9}>
+        <h2>Empresas, sociedades o asociaciones ({this.items().length})</h2>
+        <Grid container spacing={24}>
+          <Grid item sm={12}>
+            {/* box starts*/}
+            { this.items().map( (interes, i) =>
+            <Paper className="pdn_d_box" key={"interes-" + i}>
+              <Grid container spacing={24} className="row pdn_border">
+                <Grid item sm={6}>
+                  <p><span className="label declarante"> Declarante</span></p>
+                </Grid>
+                <Grid item sm={6} className="right">
+                  <a onClick={(e) => this.toggl(interes, i, e)} heref="#" className={"pdn_arrow " + (interes.show ?  "close" : "open")}></a>
+                </Grid>
+              </Grid>
               {/* row ends*/}
-              <div className="row pdn_border">
-                {/* país*/}
-                <div className="col-sm-4">
-                  <p className="pdn_label">País</p>
-                  <p className="pdn_data_p">{interes.pais_registro.valor} </p>
-                </div>
-                {/* RFC*/}
-                <div className="col-sm-4">
-                  <p className="pdn_label">R.F.C</p>
-                  <p className="pdn_data_p">{interes.rfc} </p>
-                </div>
-                {/* # registro */}
-                <div className="col-sm-4">
-                  <p className="pdn_label">Número de registro</p>
-                  <p className="pdn_data_p">{interes.numero_registro} </p>
-                </div>
+              {/* div close/open */}
+              <div style={ {display : (interes.show ? "block" : "none")} }>
+                <Grid container spacing={24} className="pdn_border">
+                  {/* empresa, sociedad o asociación */}
+                  <Grid item sm={9}>
+                    <p className="pdn_label">Nombre de la empresa, sociedad o asociación</p>
+                    <h3> { interes.nombre_empresa_sociedad_asociacion } </h3>
+                    <p className="pdn_data_p pnd_box_note pdn_gray">{interes.domicilio.vialidad.tipo_vial+' '+interes.domicilio.vialidad.nom_vial+' No.' +interes.domicilio.numExt+ ' No. Int.'+interes.domicilio.numInt}
+                     <br/> { interes.domicilio.localidad.nom_loc+', '+interes.domicilio.municipio.nom_mun+', '+interes.domicilio.entidad_federativa.nom_ent+', '+interes.domicilio.pais.valor+' C.P. '+interes.domicilio.cp } </p>
+                  </Grid>
+                  {/* constitución */}
+                  <Grid item sm={3}>
+                    <p className="pdn_label">Fecha de constitución</p>
+                    <p className="pdn_data_p">{interes.fecha_constitucion} </p>
+                  </Grid>
+                </Grid>
+                {/* row ends*/}
+
+                <Grid container spacing={24} className="pdn_border">
+                  {/* país*/}
+                  <Grid item sm={4}>
+                    <p className="pdn_label">País</p>
+                    <p className="pdn_data_p">{interes.pais_registro.valor} </p>
+                  </Grid>
+                  {/* RFC*/}
+                  <Grid item sm={4}>
+                    <p className="pdn_label">R.F.C</p>
+                    <p className="pdn_data_p">{interes.rfc} </p>
+                  </Grid>
+                  {/* # registro */}
+                  <Grid item sm={4}>
+                    <p className="pdn_label">Número de registro</p>
+                    <p className="pdn_data_p">{interes.numero_registro} </p>
+                  </Grid>
+                </Grid>
+                {/* row ends*/}
+
+                {/* table */}
+                <Grid container spacing={24} className="pdn_mobile_table">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <th>Rol</th>
+                        <th>Actividad económica</th>
+                        <th>Porcentaje de participación</th>
+                        <th>Sector o industria</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td><strong>{interes.rol}</strong></td>
+                        <td><b className= { 'pdn_' + interes.actividad_economica}></b> {interes.actividad_economica ? "Sí" : "No"} </td>
+                        <td>{interes.porcentaje_participacion}%
+                          <div className="pdn_bar_container darken">
+                            <div className="pdn_bar participacion" style={{ width: interes.porcentaje_participacion + '%' }}></div>
+                          </div>
+                        </td>
+                        <td>{interes.sector_industria.valor}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </Grid>
+                {/* table ends */}
               </div>
-              {/* table */}
-              <div className="pdn_mobile_table">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Rol</th>
-                    <th>Actividad económica</th>
-                    <th>Porcentaje de participación</th>
-                    <th>Sector o industria</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td><strong>{interes.rol}</strong></td>
-                    <td><b className= { 'pdn_' + interes.actividad_economica}></b> {interes.actividad_economica ? "Sí" : "No"} </td>
-                    <td>{interes.porcentaje_participacion}%
-                      <div className="pdn_bar_container darken">
-                        <div className="pdn_bar participacion" style={{ width: interes.porcentaje_participacion + '%' }}></div>
-                      </div>
-                    </td>
-                    <td>{interes.sector_industria.valor}</td>
-                  </tr>
-                </tbody>
-              </table>
-              </div>
-              {/* table ends */}
-            </div>
-            {/* div close/open ends */}
-        </div>
-        )}
-      </div>
-    </div>
-  </div>
+              {/* div close/open ends */}
+            </Paper>
+            )}
+          </Grid>
+        </Grid>
+      </Grid>
+      </Grid>
     );
   }
 
