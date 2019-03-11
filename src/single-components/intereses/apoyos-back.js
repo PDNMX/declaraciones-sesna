343577390d79edaa7ec8001d079b77f3893fd082
el coruco <boris@gobiernofacil.com>
2019-03-10 20:18:20 -0600

    add another item to MUI

--- src/single-components/intereses/apoyos-back.js
+++ src/single-components/intereses/apoyos-back.js
@@ -6,7 +6,6 @@
   ////////////////////////////////////////////////////////////////////////////////
 */
 import React, {Component} from "react";
-import {Typography, Grid, Button, Paper} from '@material-ui/core';
 
 /*
 	////////////////////////////////////////////////////////////////////////////////
@@ -39,74 +38,72 @@ class InteresesApoyos extends Component{
    */
   render(){
     return(
-      <Grid container spacing={24} direction={'row-reverse'} className="col-sm-offset-3 sidecontent">
-      <Grid item sm={9}>
+      <div className="col-sm-9 col-sm-offset-3 sidecontent">
         <h2>Apoyos ({this.items().length})</h2>
-        <Grid container spacing={24}>
-          <Grid item sm={12}>
+        <div className="row">
+          <div className="col-sm-12">
             {/* box starts*/}
             { this.items().map( (interes, i) =>
-            <Paper className="pdn_d_box" key={"interes-" + i}>
-              <Grid container spacing={24} className="row pdn_border">
-                <Grid item sm={6}>
+            <div className="pdn_d_box" key={"interes-" + i}>
+              <div className="row pdn_border">
+                <div className="col-sm-6">
                   <p><span className="label declarante"> Declarante</span></p>
-                </Grid>
-                <Grid item sm={6} className="right">
+                </div>
+                <div className="col-sm-6 right">
                   <a onClick={(e) => this.toggl(interes, i, e)} heref="#" className={"pdn_arrow " + (interes.show ?  "close" : "open")}></a>
-                </Grid>
-              </Grid>
+                </div>
+              </div>
               {/* row ends*/}
               {/* div close/open */}
               <div style={ {display : (interes.show ? "block" : "none")} }>
-                <Grid container spacing={24} className="pdn_border">
+                <div className="row pdn_border">
                   {/*programa*/}
-                  <Grid item sm={9}>
+                  <div className="col-sm-9">
                     <p className="pdn_label">Programa</p>
                     <h3>{interes.programa}</h3>
-                  </Grid>
+                  </div>
                   {/* valor anual*/}
-                  <Grid item sm={3}>
+                  <div className="col-sm-3">
                     <p className="pdn_label">Valor anual del apoyo</p>
                     <h3 className="pdn_amount right">${interes.valor_anual_apoyo} </h3>
-                  </Grid>
-                </Grid>
+                  </div>
+                </div>
                 {/* row ends*/}
 
-                <Grid container spacing={24} className="pdn_border">
-                  <Grid item sm={4}>
+                <div className="row pdn_border">
+                  <div className="col-sm-4">
                     <p className="pdn_label">Institución otorgante</p>
                     <p className="pdn_data_p" >{interes.institucion_otorgante}</p>
-                  </Grid>
-                  <Grid item sm={4}>
+                  </div>
+                  <div className="col-sm-4">
                     <p className="pdn_label">Nivel de Gobierno</p>
                     <p className="pdn_data_p" >{interes.nivel_orden_gobierno.valor}</p>
-                  </Grid>
-                  <Grid item sm={4}>
+                  </div>
+                  <div className="col-sm-4">
                     <p className="pdn_label">Tipo de Apoyo</p>
                     <p className="pdn_data_p" >{interes.tipo_apoyo.valor}</p>
-                  </Grid>
-                </Grid>
+                  </div>
+                </div>
                 {/* row ends*/}
 
-                <Grid container spacing={24}>
-                  <Grid item sm={3}>
+                <div className="row pdn_border">
+                  <div className="col-sm-3">
                     <p className="pdn_label">Es beneficiario</p>
                     <p className="pdn_data_p"><b className= { 'pdn_' + interes.es_beneficiario}></b> {interes.es_beneficiario ? "Sí" : "No"}</p>
-                  </Grid>
-                  <Grid item sm={9}>
+                  </div>
+                  <div className="col-sm-9">
                     <p className="pdn_label">Explicación</p>
                     <p className="pdn_data_p">{interes.observaciones}</p>
-                  </Grid>
-                </Grid>
+                  </div>
+                </div>
                 {/* row ends*/}
 
               </div>
-            </Paper>
+            </div>
             )}
-          </Grid>
-        </Grid>
-      </Grid>
-      </Grid>
+          </div>
+        </div>
+      </div>
     );
   }
 
