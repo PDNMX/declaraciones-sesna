997e047a03803d56a3a0d313598497db91382d05
hugovom <hugo.om@gmail.com>
2019-03-11 23:49:42 -0600

    update Pasivos #4

--- src/main-components/Pasivos-back.js
+++ src/main-components/Pasivos-back.js
@@ -6,8 +6,6 @@
   ////////////////////////////////////////////////////////////////////////////////
 */
 import React, {Component} from "react";
-import { Grid} from '@material-ui/core';
-
 import { Switch, Route, Link } from 'react-router-dom';
 
 // PASIVOS
@@ -32,26 +30,26 @@ class Pasivos extends Component{
 		let section = this.props.section;
 		return(
 			<div>
-			<Grid item sm={3} className="sidebar">
-				<h2>Pasivos</h2>
-				<ul>
-					<li>
-			  	  <Link className={ section == "deudas" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/pasivos/deudas`}>
-			  	    Deudas <span>{this.props.profile.pasivos.deudas.length}</span>
-			  	  </Link>
-			    </li>
-			    <li>
-			  	  <Link className={ section == "otras-obligaciones" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/pasivos/otras-obligaciones`}>
-			  	    Otras obligaciones <span>{this.props.profile.pasivos.otras_obligaciones.length}</span>
-			  	  </Link>
-			    </li>
-			  </ul>
-			</Grid>
-		  <Switch>
-				<Route exact path='/perfil/:id/pasivos' render={() => <PasivosResume profile={this.props.profile}  />}/>
-		    <Route exact path='/perfil/:id/pasivos/deudas' render={() => <PasivosDeudas profile={this.props.profile}  />}/>
-		    <Route exact path='/perfil/:id/pasivos/otras-obligaciones' render={() => <PasivosObligaciones profile={this.props.profile}  />}/>
-		  </Switch>
+			<div class="col-sm-3 sidebar">
+		<h2>Pasivos</h2>
+		<ul>
+			<li>
+	  	  <Link className={ section == "deudas" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/pasivos/deudas`}>
+	  	    Deudas <span>{this.props.profile.pasivos.deudas.length}</span>
+	  	  </Link>
+	    </li>
+	    <li>
+	  	  <Link className={ section == "otras-obligaciones" ?"router-link-exact-active router-link-active" : ""} to={`/perfil/${this.props.profile._id}/pasivos/otras-obligaciones`}>
+	  	    Otras obligaciones <span>{this.props.profile.pasivos.otras_obligaciones.length}</span>
+	  	  </Link>
+	    </li>
+	  </ul>
+	</div>
+	  <Switch>
+			<Route exact path='/perfil/:id/pasivos' render={() => <PasivosResume profile={this.props.profile}  />}/>
+	    <Route exact path='/perfil/:id/pasivos/deudas' render={() => <PasivosDeudas profile={this.props.profile}  />}/>
+	    <Route exact path='/perfil/:id/pasivos/otras-obligaciones' render={() => <PasivosObligaciones profile={this.props.profile}  />}/>
+	  </Switch>
 	  </div>
 		);
 	}
