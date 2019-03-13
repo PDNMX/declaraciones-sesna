0805adf2368ee42e0ce917981e82e16351fd41f2
el coruco <el.coruco@gmail.com>
2019-03-13 17:41:52 -0600

    add new build

---
+++ build/precache-manifest.2c80c4dad1a76459ead4b881dcc6bcdc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da2b4e459ac145bea31c",
+    "url": "/static/css/main.7fb14a54.chunk.css"
+  },
+  {
+    "revision": "da2b4e459ac145bea31c",
+    "url": "/static/js/main.da2b4e45.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "659f70f0408c56f1a9e0",
+    "url": "/static/js/2.659f70f0.chunk.js"
+  },
+  {
+    "revision": "dc6b41ed36b84f85291c7a7c978ab314",
+    "url": "/index.html"
+  }
+];
